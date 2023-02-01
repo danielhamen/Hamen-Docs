44455a3e842c105dc2d03f437ac9e0ec6ce4c9ef
danielhamen <danielhamen92@gmail.com>
2023-02-01 14:53:10 -0500

    Updated

--- app/tutorials/python-course/python-integers/index.js
+++ app/tutorials/python-course/python-integers/index.js
@@ -6,22 +6,36 @@ window.addEventListener("load", function () {
 
         // Introduction Section:
         let section = layout.newSection(); [
-            docsElements.p("In code, a string value is a sequence of characters and is used to represent text in Python"),
-            docsElements.p("A string is denoted by either single or double quotes"),
+            docsElements.p("In Python, an " + docsElements.inlineCode("int()") + " value is a whole numb27, either positive, negative, or zero"),
+            docsElements.p("Integers cannot contain decimals, whereas " + docsElements.inlineCode("float()") + " can"),
+            docsElements.p("Unlike many other languages, integers are not limited to any range such as 32-bit or 64-bit"),
+            docsElements.p("You can create an int value in Python by assigning a number without a decimal point to a variable:"),
             docsElements.codeBlock([
-                "{comment(# String:)}",
-                "{var(x)} = {str(\"Hello World!\")}",
+                "{var(x)} = {num(14)}",
+                "{var(y)} = {num(-64)}"
+            ])
+        ].forEach(elem => {
+            section.appendChild(elem);
+        }); body.appendChild(section);
+
+        // 'Creating Integers' Section:
+        section = layout.newSection("Basic Arithmetic Operations"); [
+            docsElements.p("You can perform basic arithmetic operations on int values in Python, such as addition, subtraction, multiplication, and division"),
+            docsElements.codeBlock([
+                "{var(x)} = {num(14)}",
+                "{var(y)} = {num(27)}",
+                "{var(z)} = {var(x)} + {var(y)}",
                 "",
-                "{comment(# Also a string:)}",
-                "{var(x)} = {str('Hello World!')}"
-            ], "For example"),
-            docsElements.noteText("There is absolutely no difference between defining a string with double quotes or single quotes. The reason there are 2 ways is because is most other programming languages, there is a type called: " + docsElements.inlineCode("char") + " which is defined by double quotes")
+                "{func(print)}({var(z)})",
+                "{term()}{out(34)}"
+            ]),
+            docsElements.noteText("Python follows the Order of Operations (BEDMAS) when evaluating an expression")
         ].forEach(elem => {
             section.appendChild(elem);
         }); body.appendChild(section);
 
         /* Course Navigator: */
-        let courseNavigator = docsElements.courseNavigator("../user-input/index.html", "User Input", "../python-variables/index.html", "Python: Variables");
+        let courseNavigator = docsElements.courseNavigator("../python-strings/index.html", "Python: Strings", "../python-floats/index.html", "Python: Floats");
         body.setCourseNavigator(courseNavigator);
 
         /* Footer: */
