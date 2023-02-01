964d45bb6e66ea2d5ec9c75a17f0dbb24704f0bc
BuildTools <unconfigured@null.spigotmc.org>
2023-02-01 14:58:22 -0500

    Fixed incorrect content

--- app/tutorials/python-course/python-strings/index.js
+++ app/tutorials/python-course/python-strings/index.js
@@ -15,7 +15,7 @@ window.addEventListener("load", function () {
                 "{comment(# Also a string:)}",
                 "{var(x)} = {str('Hello World!')}"
             ], "For example"),
-            docsElements.noteText("There is absolutely no difference between defining a string with double quotes or single quotes. The reason there are 2 ways is because is most other programming languages, there is a type called: " + docsElements.inlineCode("char") + " which is defined by double quotes")
+            docsElements.noteText("There is absolutely no difference between defining a string with double quotes or single quotes. The reason there are 2 ways is because is most other programming languages, there is a type called: " + docsElements.inlineCode("char") + " which is defined by single quotes")
         ].forEach(elem => {
             section.appendChild(elem);
         }); body.appendChild(section);
