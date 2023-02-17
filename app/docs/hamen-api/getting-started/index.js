118ec229af6b193af83b2ad2480ec9b424631c52
BuildTools <unconfigured@null.spigotmc.org>
2023-02-16 20:02:54 -0500

    Updated

--- app/docs/hamen-api/getting-started/index.js
+++ app/docs/hamen-api/getting-started/index.js
@@ -99,21 +99,37 @@ window.addEventListener("load", function () {
             section.appendChild(elem);
         }); body.appendChild(section);
 
-        // VSCode 'Hamen API' extension - Eg. "You'll notice that there is no auto-correct when using the HamenAPI, ou can use the HamenAPI VSCode Extension"
-        section = layout.newSection("Hamen API Text-Editor Extension"); [
-            docsElements.p("If you're using a " + docsElements.anchorText("Text-Editor", "https://www.hamen.tech/articles/chosing-the-right-text-editor", "_blank") + ", you may have some issues regarding Intelisense / suggestions when using our API"),
-            docsElements.p("Because of this, we've created Extensions for each of the following Text-Editors:"),
+        // Section:
+        section = layout.newSection("Hamen API Documentation");[
+            docsElements.p("Within the next many sections, we will cover each part of the Hamen API"),
             docsElements.unorderedList([
-                docsElements.anchorText("Visual Studio Code", "#", "_blank"),
-                docsElements.anchorText("Visual Studio", "#", "_blank"),
-                docsElements.anchorText("Sublime Text", "#", "_blank"),
-                docsElements.anchorText("Atom (depreciated)", "#", "_blank"),
-                docsElements.anchorText("Backets", "#", "_blank")
-            ]),
-            docsElements.p("With the use of our extension, you can access Intellisense, and many other useful features for ease when using our API")
+                "" + docsElements.anchorText("Arrays Expansion Pack", "../arrays/index.html", "_self"),
+                "" + docsElements.anchorText("Conversions Expansion Pack", "../conversions/index.html", "_self"),
+                "" + docsElements.anchorText("Math Expansion Pack", "../math/index.html", "_self"),
+                "" + docsElements.anchorText("Objects Expansion Pack", "../objects/index.html", "_self"),
+                "" + docsElements.anchorText("Random Expansion Pack", "../random/index.html", "_self"),
+                "" + docsElements.anchorText("Strings Expansion Pack", "../strings/index.html", "_self"),
+                "" + docsElements.anchorText("Validation Expansion Pack", "../validation/index.html", "_self")
+            ], "Additionally, you can reference the specific expansion packs below")
         ].forEach(elem => {
             section.appendChild(elem);
         }); body.appendChild(section);
+
+        // VSCode 'Hamen API' extension - Eg. "You'll notice that there is no auto-correct when using the HamenAPI, ou can use the HamenAPI VSCode Extension"
+        // section = layout.newSection("Hamen API Text-Editor Extension"); [
+        //     docsElements.p("If you're using a " + docsElements.anchorText("Text-Editor", "https://www.hamen.tech/articles/chosing-the-right-text-editor", "_blank") + ", you may have some issues regarding Intelisense / suggestions when using our API"),
+        //     docsElements.p("Because of this, we've created Extensions for each of the following Text-Editors:"),
+        //     docsElements.unorderedList([
+        //         docsElements.listItem(docsElements.anchorText("Visual Studio Code", "#", "_blank")),
+        //         docsElements.listItem(docsElements.anchorText("Visual Studio", "#", "_blank")),
+        //         docsElements.listItem(docsElements.anchorText("Sublime Text", "#", "_blank")),
+        //         docsElements.listItem(docsElements.anchorText("Atom (depreciated)", "#", "_blank")),
+        //         docsElements.listItem(docsElements.anchorText("Backets", "#", "_blank"))
+        //     ]),
+        //     docsElements.p("With the use of our extension, you can access Intellisense, and many other useful features for ease when using our API")
+        // ].forEach(elem => {
+        //     section.appendChild(elem);
+        // }); body.appendChild(section);
 
         /* Course Navigator: */
         let courseNavigator = docsElements.courseNavigator("../introduction/index.html", "Introduction to the Hamen API", "javascript:void(0);", "");
